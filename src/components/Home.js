ebc1598f852d51096da866c17251297550c2833d
Ucodify <hasasamee@gmail.com>
2025-10-07 11:15:24 +0500

    State Bank

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Shop from "./Shop";
+
+const Home = () => {
+  return (
+    <div className=''>
+      <Navbar />
+      <Shop />
+    </div>
+  );
+};
+
+export default Home;
